9f062eee5a856d7ec40e86eafc904ce7b4fe2545
jaguarhkk <abekorir3470@gmail.com>
2025-06-21 14:26:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-module.exports = {
-    username: "xQ6iEAGYhrkdqidMSSas",
-    password: "MyC4EVYGcP9aNnWOcp3YKxAqEDe6kr8Jd3X5CQVG",
-    shortcode: "1868",
-    passkey: "dummy_passkey", // Optional for PayHero (used in M-Pesa)
-    callbackUrl: "https://your-render-url.onrender.com/callback"
-};
+
